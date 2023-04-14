517e991aca2d7485aef64689a4a6b597d63cc47c
lijian <lj2690@163.com>
2023-04-14 12:35:38 +0800

    细节优化

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,19 @@ import {GridItemBean} from "./model/grid-item-bean";
 let gridInfo = reactive(new GridBean());
 gridInfo.cols = 100;//宽度分为100份
 gridInfo.rows = 100;//高度分为100份
+let childList: GridItemBean[] = [];
 let itemBean = reactive(new GridItemBean());
 itemBean.topSpan = 1;
 itemBean.leftSpan = 1;
 itemBean.widthSpan = 10;
 itemBean.heightSpan = 20;
-gridInfo.childList.push(itemBean);
+childList.push(itemBean);
 let itemBean2 = reactive(new GridItemBean());
 itemBean2.topSpan = 50;
 itemBean2.leftSpan = 50;
 itemBean2.widthSpan = 10;
 itemBean2.heightSpan = 20;
-gridInfo.childList.push(itemBean2);
+childList.push(itemBean2);
 
 setTimeout(() => {
     itemBean.topSpan = 10;
@@ -30,12 +31,12 @@ let disable = ref(false);
 <template>
     <grid-layout :info="gridInfo" style="background-color: azure">
         <template #default="item">
-            <template v-for="gridItem of gridInfo.childList">
+            <template v-for="item of childList">
                 <grid-item
                         :disable="disable"
-                        :item-info="gridItem"
+                        :item-info="item"
                         style="background-color: blueviolet">
-                    {{ gridItem }}
+                    {{ item }}
                 </grid-item>
             </template>
         </template>
--- src/model/grid-bean.ts
+++ src/model/grid-bean.ts
@@ -1,10 +1,12 @@
 import {GridItemBean} from "./grid-item-bean";
 
 class GridBean {
-    rows: number = 48;//行数
-    cols: number = 48;//列数
-    infiniteHeight: boolean = true;//无限高度
-    childList: GridItemBean[] = []; //子列表
+    //行数
+    rows: number = 48;
+    //列数
+    cols: number = 48;
+    //无限高度
+    infiniteHeight: boolean = true;
 }
 
 export {
